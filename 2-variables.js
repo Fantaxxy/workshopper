1a74576706447b7a8678fc5c4d36eee1231fd57c
Fantaxxy <byrdmanfun@gmail.com>
2022-03-12 22:31:53 -0500

    Very Able

---
+++ 2-variables.js
@@ -0,0 +1,13 @@
+let Dave
+let Sarah
+let Ernesto
+
+let color1 = "Green"
+let color2 = "Blue"
+let color3 = "mauve"
+
+// Ernesto's favorite color is Green Sarah's favorite color is Blue Dave's favorite color is Mauve
+console.log(`Ernesto's favorite color is ${color1}, ` + `Sarah's favorite color is ${color2}, ` + `Dave's favorite color is ${color3}`)
+
+// Ernesto's favorite color is actually Mauve Sarah loves Green and Dave is a fan of Blue
+console.log(`Ernesto's favorite color is ${color3}, ` + `Sarah's favorite color is ${color1} ` + `Dave's favorite color is ${color2}`)
