36ec6f57c16b36c48d309bbd726bf132992ef7bf
Fantaxxy <byrdmanfun@gmail.com>
2022-03-13 11:20:00 -0400

    Vey Able

---
+++ 3-stringy.js
@@ -0,0 +1,6 @@
+// I just bought a tubby cat and will sell it for some ETH hopefully
+let tubby = "I just bought a tubby cat and will sell it for some ETH hopefully"
+console.log(tubby.length)
+
+let myName = "JORDAN"
+console.log(myName.toLocaleLowerCase())
