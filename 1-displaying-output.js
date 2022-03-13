fc343179328071800a399e095708aa6c4a752e53
Fantaxxy <byrdmanfun@gmail.com>
2022-03-12 20:19:11 -0500

    #Displayed

---
+++ 1-displaying-output.js
@@ -0,0 +1,9 @@
+// Drake
+// Pound Cake
+console.log("Yeah, I swear shit just started clickin' dog, You know it's real when you are who you think you are")
+// Age
+console.log(parseInt("31 he is"))
+// new line
+let str = "I would like to thank\n my arms for always being\n by my side, my legs\n for always supporting me, and\n my fingers, because I can\n always count on them."
+
+console.log(str);
