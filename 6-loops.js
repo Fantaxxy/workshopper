73e7a9aba686516d908f0c9ca32dfb9d8ea11be5
Fantaxxy <byrdmanfun@gmail.com>
2022-03-14 11:21:13 -0400

    On one condition

---
+++ 6-loops.js
@@ -0,0 +1,18 @@
+// for (let i = 1; i <= 50; i++) {
+//     console.log(i)
+// }
+
+// let num = 23
+// for (let i = 1; i <= num; i++) {
+//     if (i === 24) {break; }
+//     console.log(i)
+// }
+
+let num = 23
+for (let i = 0; i < num; i++) {
+    if (i % 2 === 0){
+        console.log(i)
+    }
+     {continue; }
+    
+}
