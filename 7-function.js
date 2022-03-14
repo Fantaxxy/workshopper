55226311bab9c492e64eb4f4faba79594aabf5d4
Fantaxxy <byrdmanfun@gmail.com>
2022-03-14 11:49:50 -0400

    On one condition

---
+++ 7-function.js
@@ -0,0 +1,12 @@
+function howLong(num) {
+    // for (let i = 0; i <= num.length; i++) {
+        if (num.length >= 10) {
+            console.log(true)
+        } else {
+            console.log(false)
+            // {break ;}
+        }
+    // }
+}
+
+howLong("Calvinsgotajobyall")
